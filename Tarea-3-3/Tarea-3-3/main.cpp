86f4c4db2637d8ea49385ebd456700d63e64dd51
javicuriel <javiercuriel94@gmail.com>
2015-10-08 18:11:17 -0500

    chancla

--- Tarea-3-3/Tarea-3-3/main.cpp
+++ Tarea-3-3/Tarea-3-3/main.cpp
@@ -11,69 +11,60 @@
 
 using namespace std;
 
-int numsLCS[10][10];
-char flechasLCS[5][5];
-
-
-
-
-int findLCS(char x[],char y[]){
+template<class T>
+vector<T> findLCS(vector<T> x, vector<T> y){
     int i,j;
-    long int m = strlen(x)+1;
-    long int n = strlen(y)+1;
+    long int m = x.size()-1;
+    long int n = y.size()-1;
     int temp[m][n];
 
-    int max = 0;
     for(i = 1;i < m;i++){
         for (j = 1; j < n; j++) {
-            temp[i][j] = std::__1::max(temp[i][j-1],temp[i-1][j]);
             if(x[i-1] == y[j-1]){
                 temp[i][j] = temp[i-1][j-1] +1;
-                if(temp[i][j] >max){
-                    max = temp[i][j];
-                }
+            }
+            else{
+                temp[i][j] = max(temp[i-1][j], temp[i][j-1]);
             }
         }
     }
-    return max;
+    int tam = temp[m][n];
+    vector<T> secuencia;
+    while(m >-1 && n > -1){
+        if(x[m] == y [n]){
+            secuencia.push_back(x[m]);
+            m--;
+            n--;
+            tam--;
+        }
+        else if(x[m]>y[n]){
+            m--;
+        }
+        else{
+            n--;
+        }
+    }
+    return secuencia;
 }
-
-//int findLCS(char x[],char y[]){
-//    int i,j;
-//    long int m = strlen(x)+1;
-//    long int n = strlen(y)+1;
-//    int temp[m][n];
-//
-//    for(i = 1;i <= m;i++)
-//        numsLCS[i][0] = 0;
-//    for(i = 0;i <= n;i++)
-//        numsLCS[0][i] = 0;
-//    
-//    for(i = 1;i <= m;i++){
-//        for (j = 1; j <= n; j++) {
-//            if(x[i]==y[j]){
-//                numsLCS[i][j] = 1 + numsLCS[i-1][j-1];
-//            }
-//            else{
-//                if(numsLCS[i-1][j] >= numsLCS[i][j-1])
-//                    numsLCS[i][j] = numsLCS[i-1][j];
-//                
-//                else
-//                    numsLCS[i][j] = numsLCS[i][j-1];
-//            }
-//        }
-//    }
-//    return max;
-//}
 
 
 int main(int argc, const char * argv[]) {
-    char x[] = {'A','B','C','D','G','H','L','Q','R'};
-    char y[] = {'A','E','D','P','H','R'};
-
-    //char y[] = {'1','2'};
-    cout << findLCS(x,y);
-
+    vector<int> x = {1,2,3,4,5};
+    vector<int> y = {1,2,3};
+    vector<char> xchar = {'a','b','c','d','f'};
+    vector<char> ychar = {'a','b','d','f'};
+    vector<char> resultadochar = findLCS(xchar, ychar);
+    vector<int> resultado = findLCS(x,y);
+    cout << "Subsecuencia de Ints: ";
+    for(long int i = resultado.size()-1; i >= 0; i--){
+        cout << resultado[i];
+    }
+    cout << endl;
+    cout << "Subsecuencia de Chars: ";
+    for(long int i = resultadochar.size()-1; i >= 0; i--){
+        cout << resultadochar[i];
+    }
+    cout << endl;
     return 0;
 }
 
